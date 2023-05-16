ab39bf24825dec23e75edeeea390fd0d4e448559
WittHub <rasmus.wittrock@gmail.com>
2023-05-16 11:20:16 +0200

    spring

--- AsteroidsServiceLoader/Core/src/main/java/module-info.java
+++ AsteroidsServiceLoader/Core/src/main/java/module-info.java
@@ -2,6 +2,11 @@ module Core {
     requires com.badlogic.gdx;
     requires common;
     requires java.desktop;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
